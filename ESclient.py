45227401d950a01dbbbcf7458b8a49be71037054
volinh <volinh275@gmail.com>
2018-03-14 12:13:47 +0700

    3

--- ESclient.py
+++ ESclient.py
@@ -11,6 +11,7 @@ class ESclient(object):
         self.host = host
         self.port = port
 
+
     def get_elasticsearch_client(self):
         logging.info("get elasticsearch client")
         return Elasticsearch(
@@ -28,41 +29,65 @@ class ESclient(object):
             json_file = file.read()
         return json.loads(json_file)
 
+
     def iter_data(self,json_arr):
         for json in json_arr:
             yield json
+
 
     def insert_data(self,json_arr,index,doc_type):
         es = self.get_elasticsearch_client()
         helpers.bulk(es,index=index,doc_type=doc_type,actions=self.iter_data(json_arr),stats_only=True)
 
+
     def delete_index(self):
         pass
 
-    def scan_data(self,query):
+
+    def reindex_data(self,source_index,target_index,query):
+        logging.info("reindex data")
+        es = self.get_elasticsearch_client()
+        helpers.reindex(es,source_index=source_index, target_index=target_index, query=query)
+
+
+    def scan_data(self,index,doc_type,query):
         logging.info("scan data from sever")
         es = self.get_elasticsearch_client()
-        dict_product = {}
-        rs = helpers.scan(es, index="btl", doc_type="work", query=query, scroll="1m")
+        rs = helpers.scan(es, index=index, doc_type=doc_type, query=query, scroll="1m")
         list_rs = []
         for doc in rs:
             print(doc)
             data = doc["_source"]
             list_rs.append(data)
-
         for i in list_rs:
             print(i)
+        return list_rs
 
-    def count_data(self):
+
+    def count_data(self,index,doc_type,query):
         logging.info("count amount of data")
         es = self.get_elasticsearch_client()
-        rs = es.count(index="btl", doc_type="work", body=None)
+        rs = es.count(index=index, doc_type=doc_type, body=query)
         logging.info("so dong : {}".format(rs["count"]))
 
+
+    def search_data(self,index,doc_type,query):
+        logging.info("search")
+        es = self.get_elasticsearch_client()
+        rs = es.search(index=index, doc_type=doc_type, body=query,size="1m")
+        return rs
+
+
+    def map_data(self):
+        logging.info("map data")
+        pass
+
+
 if __name__ == "__main__":
-    client = ESclient(setting.HOST_ELASTICSEARCH,setting.PORT_ELASTICSEARCH)
-    client.count_data()
-    client.scan_data(None)
+    pass
+    # client = ESclient(setting.HOST_ELASTICSEARCH,setting.PORT_ELASTICSEARCH)
+    # client.count_data()
+    # client.scan_data(None)
     #json = client.read_json("data/carerrbuilder.json")
     #client.feed_data(json_arr=json,index="btl",doc_type="work")
     # json1 = client.read_json("data/carerrbuilder.json")
--- app.py
+++ app.py
@@ -1,16 +1,52 @@
 from elasticsearch import Elasticsearch
 from flask import Flask
+from ESclient import ESclient
 import setting
 
 app = Flask(__name__)
+
+es = ESclient(setting.HOST_ELASTICSEARCH,setting.PORT_ELASTICSEARCH)
 
 @app.route("/hello")
 def hello():
     return "hello"
 
-@app.route("/search/<param>")
-def search(param):
-    return param
+@app.route("/search/<loc>/<param>")
+def search(loc,param):
+    if loc == "content" :
+        search_content(param)
+    elif loc == "title" :
+        search_title(param)
+    elif loc == "url" :
+        search_url(param)
+    elif loc == "total" :
+        search_total(param)
+
+
+def search_content(param):
+    query = {
+        "query" : {
+            "match" : {
+                "content" : param
+            }
+        }
+    }
+    rs = es.search_data(index="btl",doc_type="work",query=query)
+    return rs
+
+
+def search_title(param):
+    pass
+
+
+def search_url(param):
+    pass
+
+
+def search_total(param):
+    pass
+
+
 
 if __name__ == "__main__":
     app.run(host=setting.HOST_APP,port=setting.PORT_APP,debug=True)
